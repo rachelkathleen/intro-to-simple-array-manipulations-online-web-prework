e921ecae7441f21ed3511f53c4b9843a4b1424ed
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 05:37:13 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,21 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, new_element)
-  array.insert(4, new_element) 
+  array.insert(4, new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete_at(int)
 end
